bcdd7ef740d9d924bd18be4a2b347b17356c7043
jphillips <josh.phillips@fearnworks.com>
2023-11-21 22:03:57 -0600

    Add run from compose

---
+++ docker/run_webui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ensure the script fails if any command fails, this will prevent silent issues with conda activation
+set -e
+
+source /miniconda/etc/profile.d/conda.sh
+
+conda activate llava
+
+cd /workspace
+
+
+python3 -m llava.serve.gradio_web_server --controller http://controller:10000 --model-list-mode reload --port 11000
